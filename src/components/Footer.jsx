04f8391e7f09c2a5be7300c3270192f781e58c65
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:43:17 +0100

    feat: render footer navigation links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,2 +1,11 @@
 import React from 'react';
 
+export default function Footer() {
+  return (
+    <footer className="app-footer">
+      <div className="footer-links">
+        <a href="/docs">Documentation</a>
+        <a href="https://github.com/teefeh-07/tipjar" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="/docs/contributing">Contributing</a>
+        <a href="https://explorer.hiro.so" target="_blank" rel="noopener noreferrer">Stacks Explorer</a>
+      </div>
