0505c2c3eebeeb51df750c7ee423fa7325c6cd43
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:42:54 +0100

    feat: render header with branding section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,10 @@
 import React from 'react';
 import ConnectWallet from './ConnectWallet.jsx';
 
+export default function Header() {
+  return (
+    <header className="app-header">
+      <div className="header-brand">
+        <h1 className="logo">Tipjar</h1>
+        <span className="tagline">Decentralized Tipping on Stacks</span>
+      </div>
