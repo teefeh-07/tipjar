019e8e5bc87da154f057343e846c9bc99e9978d1
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:40:49 +0100

    feat: implement authenticate function with showConnect

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,3 +6,17 @@ import { AppConfig, UserSession, showConnect } from '@stacks/connect';
 const appConfig = new AppConfig(['store_write', 'publish_data']);
 const userSession = new UserSession({ appConfig });
 
+export function authenticate() {
+  showConnect({
+    appDetails: {
+      name: 'Tipjar',
+      icon: window.location.origin + '/logo.png',
+    },
+    redirectTo: '/',
+    onFinish: () => {
+      window.location.reload();
+    },
+    userSession,
+  });
+}
+
