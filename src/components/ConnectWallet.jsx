d682c62f9aeef36b1b276fec57438552669d757b
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:41:42 +0100

    feat: import walletconnect service into ConnectWallet

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -1,2 +1,4 @@
 import React, { useState } from 'react';
 import { authenticate, logout, isAuthenticated, getStxAddress } from '../services/authService.js';
+import { connectWallet, disconnectWallet, isConnected } from '../services/walletConnectService.js';
+
