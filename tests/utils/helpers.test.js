129b94e06bcff93969da83f24baec7fb8ccb256a
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:49:50 +0100

    test: add micro-STX to STX conversion tests

--- tests/utils/helpers.test.js
+++ tests/utils/helpers.test.js
@@ -1,3 +1,14 @@
 // Tests for helper utilities
 import { describe, it, expect } from 'vitest';
 
+describe('Helper Utils', () => {
+  describe('microToStx', () => {
+    it('should convert 1000000 micro-STX to 1 STX', () => {
+      expect(1000000 / 1000000).toBe(1);
+    });
+
+    it('should handle zero', () => {
+      expect(0 / 1000000).toBe(0);
+    });
+  });
+
