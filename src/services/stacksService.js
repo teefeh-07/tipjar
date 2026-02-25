4df65f77019a912c9c9793561243eb92b04b6c6c
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:39:05 +0100

    feat: import stacks transactions SDK functions

--- src/services/stacksService.js
+++ src/services/stacksService.js
@@ -1,3 +1,16 @@
 // Stacks blockchain interaction service
 // Wraps @stacks/transactions and @stacks/connect
 
+import {
+  makeSTXTokenTransfer,
+  makeContractCall,
+  callReadOnlyFunction,
+  broadcastTransaction,
+  AnchorMode,
+  PostConditionMode,
+  uintCV,
+  principalCV,
+  standardPrincipalCV,
+  bufferCVFromString,
+} from '@stacks/transactions';
+
