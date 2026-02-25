549815cc55d2dec20440e7c0997758f72315f4d4
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:48:14 +0100

    test: define test wallet addresses

--- tests/tipjar.test.js
+++ tests/tipjar.test.js
@@ -1,3 +1,7 @@
 // Tests for tipjar.clar smart contract
 import { describe, it, expect, beforeEach } from 'vitest';
 
+const deployer = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
+const wallet1 = 'ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5';
+const wallet2 = 'ST2CY5V39NHDPWSXMW9QDT3HC3GD6Q6XX4CFRK9AG';
+
