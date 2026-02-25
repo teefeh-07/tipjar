22cbce0d021a70b1a9cbf75c7e51adc02a261ac1
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:41:16 +0100

    feat: add tipSent notification emitter

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -19,3 +19,7 @@ function notify(notification) {
   listeners.forEach(cb => cb(notification));
 }
 
+export function tipSent(recipient, amount) {
+  notify({ type: 'success', title: 'Tip Sent!', message: `You sent ${amount / 1000000} STX to ${recipient.slice(0, 8)}...`, timestamp: Date.now() });
+}
+
