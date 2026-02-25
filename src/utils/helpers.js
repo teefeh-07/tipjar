7680c4f4a2410b0062aa7cb050f1e0dad338e072
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:47:00 +0100

    feat: add STX and micro-STX conversion functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,3 +2,11 @@
 
 import { MICRO_STX } from './constants.js';
 
+export function microToStx(microStx) {
+  return Number(microStx) / MICRO_STX;
+}
+
+export function stxToMicro(stx) {
+  return Math.floor(Number(stx) * MICRO_STX);
+}
+
