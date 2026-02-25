cda06aa2edb89cae4c8a055eaa41ece4785afd2d
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:19:08 +0100

    feat: define walletconnect project metadata

--- src/config/walletConnect.js
+++ src/config/walletConnect.js
@@ -2,3 +2,10 @@
 
 export const WC_PROJECT_ID = process.env.WALLETCONNECT_PROJECT_ID || '';
 
+export const WC_METADATA = {
+  name: 'Tipjar',
+  description: 'Decentralized tipping platform built on Stacks',
+  url: 'https://tipjar.app',
+  icons: ['https://tipjar.app/icon.png'],
+};
+
