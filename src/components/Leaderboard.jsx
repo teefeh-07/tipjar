7a7a54a9f14c7d072d5ae49d69f502c6cec55986
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:44:09 +0100

    feat: compute leaderboard data and max amount

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,3 +1,7 @@
 import React from 'react';
 import { getTopRecipients } from '../services/tipService.js';
 
+export default function Leaderboard() {
+  const topRecipients = getTopRecipients(10);
+  const maxAmount = topRecipients.length > 0 ? topRecipients[0].total : 1;
+
