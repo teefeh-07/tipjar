dad317ca412fb25814a7e717db21348a9a739d62
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:39:31 +0100

    feat: import walletconnect packages and config

--- src/services/walletConnectService.js
+++ src/services/walletConnectService.js
@@ -1,3 +1,7 @@
 // WalletConnect integration service
 // Multi-wallet support via WalletConnect protocol
 
+import SignClient from '@walletconnect/sign-client';
+import { WalletConnectModal } from '@walletconnect/modal';
+import { WC_PROJECT_ID, WC_METADATA, WC_REQUIRED_NAMESPACES } from '../config/walletConnect.js';
+
