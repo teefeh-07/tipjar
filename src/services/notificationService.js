73662a1754edccae1161acaa5f4f96031d620047
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:41:15 +0100

    feat: implement subscribe function with unsubscribe return

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -5,3 +5,11 @@ const listeners = [];
 const notificationQueue = [];
 const MAX_QUEUE_SIZE = 50;
 
+export function subscribe(callback) {
+  listeners.push(callback);
+  return () => {
+    const index = listeners.indexOf(callback);
+    if (index > -1) listeners.splice(index, 1);
+  };
+}
+
