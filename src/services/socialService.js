95c8dc2683e9f9d4e2b275b360673b3dddaea12a
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 15:03:56 +0100

    feat: define social platform configurations

--- src/services/socialService.js
+++ src/services/socialService.js
@@ -1,2 +1,24 @@
 // Social Sharing and Tip Receipt Generation Service
 
+import { truncateAddress, formatStx } from '../utils/helpers.js';
+import { APP_NAME } from '../utils/constants.js';
+
+// Social platform configurations
+const PLATFORMS = {
+  twitter: {
+    name: 'Twitter/X',
+    shareUrl: 'https://twitter.com/intent/tweet',
+    maxLength: 280,
+  },
+  farcaster: {
+    name: 'Farcaster',
+    shareUrl: 'https://warpcast.com/~/compose',
+    maxLength: 320,
+  },
+  discord: {
+    name: 'Discord',
+    shareUrl: null, // Copy to clipboard for Discord
+    maxLength: 2000,
+  },
+};
+
