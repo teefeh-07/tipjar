7e0a8ffc12cc32f129eb420e602c866f339fd72e
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:44:58 +0100

    feat: get DOM container and create React root

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
