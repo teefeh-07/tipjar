44e0b9d2901a283b911c9dbbbcf86767da2f1596
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:46:34 +0100

    feat: define contract address and name constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,7 @@
 // Application-wide constants
 
+export const CONTRACT_ADDRESS = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
+export const TIPJAR_CONTRACT = 'tipjar';
+export const TOKEN_CONTRACT = 'tip-token';
+export const REGISTRY_CONTRACT = 'tip-registry';
+
