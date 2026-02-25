4b3e6f3a495407723e3fff247566bc71dec8fec2
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:42:31 +0100

    feat: add empty state for tip history

--- src/components/TipHistory.jsx
+++ src/components/TipHistory.jsx
@@ -1,3 +1,10 @@
 import React from 'react';
 import { getTipHistory } from '../services/tipService.js';
 
+export default function TipHistory() {
+  const tips = getTipHistory();
+
+  if (tips.length === 0) {
+    return <div className="tip-history-empty"><p>No tips sent yet. Be the first to tip a creator!</p></div>;
+  }
+
