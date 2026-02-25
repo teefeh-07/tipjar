ca3a14d35084a305af1d4c045a3133e9eeedf279
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:42:09 +0100

    feat: define tip amount preset buttons

--- src/components/SendTip.jsx
+++ src/components/SendTip.jsx
@@ -1,3 +1,10 @@
 import React, { useState } from 'react';
 import { processTip, validateTip } from '../services/tipService.js';
 
+const AMOUNT_PRESETS = [
+  { label: '0.1 STX', value: 100000 },
+  { label: '0.5 STX', value: 500000 },
+  { label: '1 STX', value: 1000000 },
+  { label: '5 STX', value: 5000000 },
+];
+
