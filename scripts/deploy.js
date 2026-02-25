cdfddde99dd3d70c8b183ba51c97bbf4c70a2982
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:53:29 +0100

    feat: import deployment dependencies

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,8 @@
 // Contract deployment script
 // Automates Clarity contract deployment to Stacks
 
+import fs from 'fs';
+import path from 'path';
+import { makeContractDeploy, broadcastTransaction, AnchorMode } from '@stacks/transactions';
+import { StacksTestnet } from '@stacks/network';
+
