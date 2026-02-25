abefd87efcb71517beb723c489db9b67a90c7f94
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:40:49 +0100

    feat: initialize app config and user session

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,3 +3,6 @@
 
 import { AppConfig, UserSession, showConnect } from '@stacks/connect';
 
+const appConfig = new AppConfig(['store_write', 'publish_data']);
+const userSession = new UserSession({ appConfig });
+
