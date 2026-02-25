29244aa98b68822b65ecf9bf59abe15b4d164022
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:40:24 +0100

    feat: define tip history store and limits

--- src/services/tipService.js
+++ src/services/tipService.js
@@ -3,3 +3,7 @@
 
 import { sendTip as stacksSendTip, getTipAmount } from './stacksService.js';
 
+const tipHistory = [];
+const TIP_MIN = 1000; // 0.001 STX in micro-STX
+const TIP_MAX = 100000000; // 100 STX
+
