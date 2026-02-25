0104666ca077befb70ccdbc821e74095d6beb01b
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:44:34 +0100

    feat: import layout and dashboard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,4 @@
 import React, { useEffect } from 'react';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Dashboard from './components/Dashboard.jsx';
