f3df2f9d8a9730d9e7a5c545317f2be090b0d09e
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:48:14 +0100

    test: add valid tip amount acceptance test

--- tests/tipjar.test.js
+++ tests/tipjar.test.js
@@ -5,3 +5,10 @@ const deployer = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
 const wallet1 = 'ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5';
 const wallet2 = 'ST2CY5V39NHDPWSXMW9QDT3HC3GD6Q6XX4CFRK9AG';
 
+describe('Tipjar Contract', () => {
+  describe('send-tip', () => {
+    it('should accept a valid tip amount', () => {
+      const amount = 1000000;
+      expect(amount).toBeGreaterThan(0);
+    });
+
