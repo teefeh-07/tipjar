5cf368c9fd20c3eb39ebd73c8b657356fb2808d0
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:18:39 +0100

    feat: define network configuration map

--- src/config/network.js
+++ src/config/network.js
@@ -1,3 +1,9 @@
 // Network configuration for Stacks blockchain
 import { StacksMainnet, StacksTestnet, StacksDevnet } from '@stacks/network';
 
+export const NETWORK_CONFIG = {
+  mainnet: new StacksMainnet(),
+  testnet: new StacksTestnet(),
+  devnet: new StacksDevnet(),
+};
+
