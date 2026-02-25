71ac17a906475ceb624daea93f5d3c2beac776d9
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-25 14:46:35 +0100

    feat: define STX unit conversion and tip limit constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,9 @@ export const TIPJAR_CONTRACT = 'tipjar';
 export const TOKEN_CONTRACT = 'tip-token';
 export const REGISTRY_CONTRACT = 'tip-registry';
 
+export const MICRO_STX = 1000000;
+export const MIN_TIP_STX = 0.001;
+export const MAX_TIP_STX = 100;
+export const MIN_TIP_MICRO = MIN_TIP_STX * MICRO_STX;
+export const MAX_TIP_MICRO = MAX_TIP_STX * MICRO_STX;
+
